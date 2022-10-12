30a190e0a4f85601bf32a792e82e97c26597e95e
Gabriele <gabrimassi@rocketmail.com>
2022-10-12 19:07:10 +0200

    Working base logic

--- etchSketch.js
+++ etchSketch.js
@@ -1,8 +1,18 @@
-const globalContainer=document.querySelector(".etchContainer");
+const globalContainer=document.querySelector('.etchContainer');
 let newDiv, pixInput=16; //pixInput must be an user input, with standard value of 16
 let pixNum=pixInput*pixInput;
 for(let i=0; i<pixNum; i++){
-    newDiv=document.createElement("div");
-    newDiv.textContent=i;
+    newDiv=document.createElement('div');
+    //newDiv.textContent=i;
+    newDiv.setAttribute('id', 'pix');
     globalContainer.appendChild(newDiv);
+}
+
+const gridChild=document.querySelectorAll('#pix');
+gridChild.forEach((div)=>{
+    div.addEventListener('mouseover', bkColor);
+});
+
+function bkColor(e){
+    e.target.style.backgroundColor="black";
 }
